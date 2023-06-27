00b3f9a2787f17bf1d6ccdc9a3b0f417ac1dd73a
ricovos123 <32581487+ricovos123@users.noreply.github.com>
2023-06-27 18:25:37 +0200

    created navbar and started with homepage

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
+import {PortfolioComponent} from "./portfolio/portfolio.component";
 
 
 const routes: Routes = [
@@ -8,6 +11,23 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'home',
+    redirectTo: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+  }
 ];
 
 @NgModule({
