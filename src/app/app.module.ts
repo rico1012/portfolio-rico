e8d400446633ea5e0baa808a0187c7514e091d67
ricovos123 <32581487+ricovos123@users.noreply.github.com>
2023-06-27 14:30:04 +0200

    start of navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MyMaterialModule} from "./my-material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    MyMaterialModule,
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/my-material.module.ts
@@ -0,0 +1,39 @@
+import { NgModule }                 from '@angular/core';
+import { MatToolbarModule }         from '@angular/material/toolbar';
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatTableModule} from "@angular/material/table";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule, MatIconButton} from "@angular/material/button";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatChipGrid, MatChipsModule} from "@angular/material/chips";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+
+
+const materialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule,
+  MatDialogModule,
+  MatTableModule,
+  MatProgressSpinnerModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatButtonModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatChipsModule,
+  MatAutocompleteModule
+]
+
+@NgModule({
+  imports: materialModules,
+  exports: materialModules
+})
+export class MyMaterialModule
+{
+}
